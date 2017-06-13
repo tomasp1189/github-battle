5ad0d3201052c70b057f968896fd32743ed86296
Tomas <tomasp1189@gmail.com>
2017-06-13 11:07:43 -0300

    Commit from DESARROLLO-PC Life Cycle Events

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,25 +1,55 @@
 var React = require('react'); 
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
-class SelectLanguage extends React.Component{
-    render(){        
-        var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-        return(
-            <ul className='languages'>
-                {languages.map(function(lang){
-                        return(
-                            <li 
-                            style={lang === this.props.selectedLanguage ? { color : '#d0021b' } : null}
-                            onClick={this.props.onSelect.bind(null, lang)}
-                            key={lang}>
-                                {lang}
+function SelectLanguage(props) {
+    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+    return (
+        <ul className='languages'>
+            {languages.map(function (lang) {
+                return (
+                    <li
+                        style={lang === props.selectedLanguage ? { color: '#d0021b' } : null}
+                        onClick={props.onSelect.bind(null, lang)}
+                        key={lang}>
+                        {lang}
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+function RepoGrid(props){
+    return(
+        <ul className='popular-list'>
+            {props.repos.map(function(repo, index){
+                return (
+                    <li key={repo.name} className='popular-item'>
+                        <div className='popular-rank'>#{index + 1}</div>
+                        <ul className='space-list-items'>
+                            <li>
+                                <img
+                                    className='avatar'
+                                    src={repo.owner.avatar_url}
+                                    alt={'Avatar for ' + repo.name}
+                                />
                             </li>
-                        )
-                    }
-                , this)}
-            </ul>
-        )
-    }
+                            <li>
+                                <a href={repo.html_url}>{repo.name}</a>
+                            </li>
+                            <li>@{repo.owner.login}</li>
+                            <li>{repo.stargazers_count} stars</li>
+                        </ul>
+                    </li>
+                )
+            })}    
+        </ul>    
+    )
+}
+
+RepoGrid.propTypes = {
+    repos: PropTypes.array.isRequired
 }
 
 SelectLanguage.propTypes = {
@@ -31,16 +61,31 @@ class Popular extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos: null
         };
+
         this.updateLanguage = this.updateLanguage.bind(this);
+    }
+    componentDidMount(){
+        this.updateLanguage(this.state.selectedLanguage);
     }
     updateLanguage(lang){
         this.setState(function() {
             return {
-                selectedLanguage: lang 
+                selectedLanguage: lang,
+                repos: null
             }
         });
+
+        api.fetchPopularRepos(lang)
+            .then(function(repos){
+                this.setState(function(){
+                    return{
+                        repos: repos
+                    }
+                })
+            }.bind(this))
     }
     render() {
         return(
@@ -49,6 +94,10 @@ class Popular extends React.Component{
                     selectedLanguage={this.state.selectedLanguage}
                     onSelect={this.updateLanguage}
                 />
+                {!this.state.repos 
+                    ? <p>LOADING</p>
+                    : <RepoGrid repos={this.state.repos}/>
+                }
             </div>
         )
     }
